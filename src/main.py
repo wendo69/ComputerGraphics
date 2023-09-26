64c4d884fb8e483fd44a2d8dfcac3bd054f9b471
shanelle-h <ashygene05@gmail.com>
2023-09-26 18:06:49 +0300

    Update main.py
    adding the loading data set function to main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
-""
+"""
 this is the main file for the project.
-""
+"""
 import pandas as pd
+
+from loadingdataset import load_massive_dataset
+
+if __name__ == "__main__":
+    massive_dataset = load_massive_dataset()
+    # Perform operations on the combined dataset as needed
+
 
 
